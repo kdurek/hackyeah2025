76b8ba595c48b10734145b0f0cb1309db145ffd5
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2025-10-05 11:39:57 +0200

    feat: add date-fns for date formatting and enhance grid component with updated actor alignment and timestamp display

--- apps/web/src/components/grid/grid.tsx
+++ apps/web/src/components/grid/grid.tsx
@@ -1,5 +1,6 @@
-/** biome-ignore-all lint/performance/noImgElement: <explanation> */
+/** biome-ignore-all lint/performance/noImgElement: false positive */
 import { useQuery } from "@tanstack/react-query";
+import { formatDistanceToNowStrict } from "date-fns";
 import { ACTOR_TYPES_COLORS } from "@/const/const";
 import type { useSimulatedActorsReturnType } from "@/hooks/useSimulatedActors";
 import { useIsMock } from "@/pages/index";
@@ -22,9 +23,24 @@ const getActorType = (type: ActorType) => {
   }
 };
 
+const getActorAlignment = (alignment: ActorAlignment) => {
+  switch (alignment) {
+    case "HOSTILE":
+      return "Hostile";
+    case "NEUTRAL":
+      return "Neutral";
+    case "UNKNOWN":
+      return "Unknown";
+    case "FRIENDLY":
+      return "Friendly";
+    default:
+      return "Unknown";
+  }
+};
+
 const getActorIcon = (type: ActorType, alignment: ActorAlignment) => {
   const imageSrc = `${type.toLowerCase()}-${alignment.toLowerCase()}.png`;
-  return <img alt="drone" height={24} src={imageSrc} width={24} />;
+  return <img alt="drone" height={32} src={imageSrc} width={32} />;
 };
 
 const Grid = ({ actors }: { actors: useSimulatedActorsReturnType }) => {
@@ -59,19 +75,26 @@ const Grid = ({ actors }: { actors: useSimulatedActorsReturnType }) => {
   return (
     <div className="mx-auto grid w-[700px] grid-cols-2 gap-x-9 gap-y-[18px] overflow-y-scroll border-white border-y-2 py-4">
       {actors.data.map((item) => (
-        <div className="flex h-[52px] gap-2" key={item.id}>
+        <div className="flex h-16 gap-2" key={item.id}>
           <div
-            className="flex size-[52px] items-center justify-center border border-white"
+            className="flex size-16 items-center justify-center border border-white"
             style={{ color: ACTOR_TYPES_COLORS[item.alignment] }}
           >
             {getActorIcon(item.type, item.alignment)}
           </div>
-          <div className="flex flex-grow flex-col border border-white p-2">
-            <div className="font-medium text-sm">{getActorType(item.type)}</div>
-            <div className="text-xs">{item.alignment}</div>
+          <div className="flex flex-grow items-center border border-white px-2">
+            <div className="flex flex-col">
+              <div className="font-medium">{getActorType(item.type)}</div>
+              <div className="text-xs">{getActorAlignment(item.alignment)}</div>
+              <div className="text-xs">
+                {formatDistanceToNowStrict(item.updatedAt, {
+                  addSuffix: true,
+                })}
+              </div>
+            </div>
           </div>
-          <div className="flex size-[52px] items-center justify-center border border-white p-2">
-            <img alt="compass" height={24} src="/compass.png" width={24} />
+          <div className="flex size-16 items-center justify-center border border-white p-2">
+            <img alt="compass" height={32} src="/compass.png" width={32} />
           </div>
         </div>
       ))}
